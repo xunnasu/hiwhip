3a67a5874157160b47448ff2f0b80d3b89376ee4
wanghaiqi <593042110@qq.com>
2021-08-22 10:35:01 +0800

    init

---
+++ src/Home/index.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper ('.swiper-container', {
+  direction: 'horizontal',
+  loop: true,
+  autoplay: 5000,
+  centeredSlides:true,
+  spaceBetween: 20,
+  slidesOffsetBefore : 200,
+  slidesOffsetAfter  : 200,
+  slidesPerView:2,
+})       
